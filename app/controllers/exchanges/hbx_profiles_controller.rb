a0efb2f773ec937ed20e731dfcb8024d9b1b4f22
Phillip Jacowski <phillipjacowski@gmail.com>
2016-08-01 18:37:50 -0500

    update datatables_helper with latest extensions refs #7968

--- app/controllers/exchanges/hbx_profiles_controller.rb
+++ app/controllers/exchanges/hbx_profiles_controller.rb
@@ -1,9 +1,7 @@
 class Exchanges::HbxProfilesController < ApplicationController
   include DataTablesAdapter
   include DataTablesSorts
-  include DataTablesSorts::VerificationsIndexSorts
   include DataTablesFilters
-  include DataTablesFilters::EmployerInvoicesIndexFilters
   
   before_action :check_hbx_staff_role, except: [:request_help, :show, :assister_index, :family_index]
   before_action :set_hbx_profile, only: [:edit, :update, :destroy]
