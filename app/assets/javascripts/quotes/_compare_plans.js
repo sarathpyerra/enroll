1f85634b6280bb7d12a2fdc3e2796f8377f34f1e
jfinucane <jimway@gmail.com>
2016-07-31 23:33:22 -0400

    Create Module QuotePageLoad

--- app/assets/javascripts/quotes/_compare_plans.js
+++ app/assets/javascripts/quotes/_compare_plans.js
@@ -18,21 +18,56 @@ QuoteComparePlans = ( function() {
       data: {plans: plans, sort_by: sort_by.substring(0, sort_by.length-2)},
       success: function(response) {
         $('#plan_comparison_frame').html(response);
-        compare_plans_listeners();
-        export_compare_plans_listener();
+        $('#compare_plans_table').dragtable({dragaccept: '.movable'});
+        $('.cost_sort').on('click', sort_plans);
+        _export_compare_plans_listener();
         $('#plan_ids').val(null);
       }
     })
   }
-  var compare_plans_listeners = function (){
-    $('#compare_plans_table').dragtable({dragaccept: '.movable'});
-    $('.cost_sort').on('click', sort_plans);
-  }
-  var compared_plans_export = function(){
+  var _compared_plans_export = function(){
     $.get('/broker_agencies/quotes/export_to_pdf');
   }
-  var export_compare_plans_listener = function(){
-    $('#pdf_export_compare_plans').on('click', compared_plans_export);
+
+  var _compared_plans_export = function(){
+    $.get('/broker_agencies/quotes/export_to_pdf');
+  }
+  var _export_compare_plans_listener = function(){
+    $('#pdf_export_compare_plans').on('click', _compared_plans_export);
+  }
+
+  var _open_quote = function() {
+    $('[aria-controls="quote-mgmt"]').attr('aria-expanded', false)
+    $('#quote-mgmt').removeClass('in')
+    $('[aria-controls="feature-mgmt"]').attr('aria-expanded', false)
+    $('#feature-mgmt').removeClass('in')
+    $('[aria-controls="plan-selection-mgmt"]').attr('aria-expanded', false)
+    $('#plan-selection-mgmt').removeClass('in')
+    $('[aria-controls="dental-plan-selection-mgmt"]').attr('aria-expanded', false)
+    $('#dental-plan-selection-mgmt').removeClass('in')
+    $('[aria-controls="publish-quote"]').attr('aria-expanded', true)
+    $('#publish-quote').addClass('in')
+  }
+  var _load_quote_listeners= function() {
+    console.log('looad')
+    $('.publish td').on('click', function(){
+        td = $(this)
+        quote_id=$('#quote_id').val()
+        plan_id=td.parent().attr('id')
+        benefit_group_id = $('#benefit_group_select').val()
+        elected=td.index()
+        cost = td.html()
+        console.log(cost, cost.text)
+        inject_quote(quote_id, benefit_group_id, plan_id, elected, cost)
+        $.ajax({
+          type: 'GET',
+          data: {quote_id: quote_id},
+          url: '/broker_agencies/quotes/get_quote_info.js'
+        }).done(function(response){
+          set_quote_toolbar(response['summary'])
+        })
+        _open_quote()
+    })
   }
   var get_health_cost_comparison =function(){
     var plans = selected_plans();
@@ -50,7 +85,7 @@ QuoteComparePlans = ( function() {
       },
       success: function(response) {
         $('#plan_comparison_frame').html(response);
-        load_quote_listeners();
+        _load_quote_listeners();
       }
     })
   }
--- app/assets/javascripts/quotes/quote.js
+++ app/assets/javascripts/quotes/quote.js
@@ -13,69 +13,18 @@ function inject_quote(quote_id, benefit_group_id, plan_id, elected, cost) {
       }
     })    
 }
-  function _open_quote() {
-    $('[aria-controls="quote-mgmt"]').attr('aria-expanded', false)
-    $('#quote-mgmt').removeClass('in')
-    $('[aria-controls="feature-mgmt"]').attr('aria-expanded', false)
-    $('#feature-mgmt').removeClass('in')
-    $('[aria-controls="plan-selection-mgmt"]').attr('aria-expanded', false)
-    $('#plan-selection-mgmt').removeClass('in')
-    $('[aria-controls="dental-plan-selection-mgmt"]').attr('aria-expanded', false)
-    $('#dental-plan-selection-mgmt').removeClass('in')
-    $('[aria-controls="publish-quote"]').attr('aria-expanded', true)
-    $('#publish-quote').addClass('in')
-  }
-function load_quote_listeners() {
-    console.log('looad')
-    $('.publish td').on('click', function(){
-        td = $(this)
-        quote_id=$('#quote_id').val()
-        plan_id=td.parent().attr('id')
-        benefit_group_id = $('#benefit_group_select').val()
-        elected=td.index()
-        cost = td.html()
-        console.log(cost, cost.text)
-        inject_quote(quote_id, benefit_group_id, plan_id, elected, cost)
-        $.ajax({
-          type: 'GET',
-          data: {quote_id: quote_id},
-          url: '/broker_agencies/quotes/get_quote_info.js'
-        }).done(function(response){
-          set_quote_toolbar(response['summary'])
-        })
-        _open_quote()
-    })
-}
+
 function set_quote_toolbar(summary) {
   $('#quote-name').html(summary['name'])
   $('#quote-status').html(summary['status'])
   $('#quote-plan-name').html(summary['plan_name'])
   $('#quote-dental-plan-name').html(summary['dental_plan_name'])
 }
-function quote_change(quote_id, benefit_group_id){
-       console.log('also', quote_id, benefit_group_id )
-       console.log('change',$("#quote_id").val())
-       if(quote_id == 'No quote'){return}
-        $.ajax({
-          type: 'GET',
-          data: {quote_id: quote_id, benefit_group_id: benefit_group_id},
-          url: '/broker_agencies/quotes/get_quote_info.js'
-        }).done(function(response){
-            window.relationship_benefits = response['relationship_benefits']
-            window.roster_premiums = response['roster_premiums']
-            QuoteManagePlans.turn_off_criteria()
-
-            set_plan_costs()
-            inject_quote(quote_id, benefit_group_id)
-            page_load_listeners()
-            set_quote_toolbar(response['summary'])
-            slider_listeners()
-             deductible_value = parseInt(response['summary']['deductible_value'])
-            $('#ex1').bootstrapSlider('setValue', deductible_value)
-            $('#ex1_input').val(deductible_value)
-            QuoteManagePlans.toggle_plans(response['criteria'])
-            set_benefits()
-        })
+function show_benefit_group(quote_id, benefit_group_id){
+  QuotePageLoad.configure_benefit_group(quote_id, benefit_group_id)
+  inject_quote(quote_id, benefit_group_id)
+  QuotePageLoad.page_load_listeners()
+  slider_listeners()
     }
 function viewDetails($thisObj) {
   if ( $thisObj.hasClass('view') ) {
