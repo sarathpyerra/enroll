28174bba57f4f01e8c453f35317e6fea8c84773f
Andrej Rasevic <andrej@rasevicengineering.com>
2016-08-25 22:23:38 -0400

    Refs #2830 fixed 5 more failing cucumber tests

--- features/step_definitions/new_hire_steps.rb
+++ features/step_definitions/new_hire_steps.rb
@@ -64,7 +64,7 @@ When(/(.*) clicks continue on the group selection page/) do |named_person|
   if find_all('.interaction-click-control-continue').any?
     find('.interaction-click-control-continue').click
   else
-    find('btn.btn-default.pull-right.interaction-click-control-shop-for-plans', :wait => 10).click
+    find('.interaction-click-control-shop-for-new-plan', :wait => 10).click
   end
   screenshot("Testing")
 end
