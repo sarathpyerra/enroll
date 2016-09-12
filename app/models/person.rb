17c30915a2c093f6a3513c1096f5ee8b13c264bc
vinner <vineelsonnathi@users.noreply.github.com>
2016-09-11 23:16:01 -0400

    Refs #2819

--- app/models/person.rb
+++ app/models/person.rb
@@ -498,7 +498,7 @@ class Person
   end
 
   def active_employee_roles
-    employee_roles.select{|employee_role| employee_role.census_employee.present? }
+    employee_roles.select{|er| er.census_employee.present? && er.census_employee.is_active? }
   end
 
   def linked_active_employee_roles
