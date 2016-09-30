e455ff2fbd9ecaf1725aae760b41f34a6b44f25f
Andrej Rasevic <andrej@rasevicengineering.com>
2016-09-30 13:53:54 -0400

    Refs #5395 added spec for date-picker helper

--- spec/helpers/time_helper_spec.rb
+++ spec/helpers/time_helper_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 
 RSpec.describe TimeHelper, :type => :helper do
   let(:person) { FactoryGirl.create(:person, :with_consumer_role) }
-  let(:hbx_enrollment) { FactoryGirl.create(:hbx_enrollment) }
 
   describe "time remaining in words" do
     it "counts 90 days from user creating date" do
@@ -10,10 +9,12 @@ RSpec.describe TimeHelper, :type => :helper do
     end
   end
 
-  describe "earliest date for terminating enrollment" do
-    it "counts -6 days from enrollment effective date"
-      hbx_enrollment.effective_on = TimeKeeper.date_of_record - 7.days
-      hbx_enrollment.save
-      expect(set_date_min_to_effective_on(hbx_enrollment, TimeKeeper.date_of_record)).to eq("-6D")
+  describe "set earliest date for terminating enrollment" do
+    let(:family) { FactoryGirl.create(:family, :with_primary_family_member)}
+    let(:enrollment) {FactoryGirl.create(:hbx_enrollment, household: family.active_household)}
+    it "counts -7 days from enrollment effective date" do
+      enrollment.effective_on = (TimeKeeper.date_of_record - 7.days)
+      expect(helper.set_date_min_to_effective_on(enrollment, TimeKeeper.date_of_record)).to eq("-7D")
     end
   end
+end
