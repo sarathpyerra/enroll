16575de6700eea17b071976dd75958ef245a0538
Zoheb Nensey <zoheb.nensey@gmail.com>
2016-11-01 14:37:36 -0400

    Refs #3919 Added first scenario

--- features/step_definitions/edit_gender_steps.rb
+++ features/step_definitions/edit_gender_steps.rb
@@ -1,13 +1,23 @@
 Given(/^I am a consumer$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  person_all = FactoryGirl.create(:person, :with_family, :with_consumer_role, :with_employee_role, :male)
+  family_all = person_all.primary_family
+  FactoryGirl.create(:hbx_profile, :no_open_enrollment_coverage_period, :ivl_2015_benefit_package)
+  qle_all = FactoryGirl.create(:qualifying_life_event_kind, market_kind: "shop")
+  FactoryGirl.create(:special_enrollment_period, family: family_all, effective_on_kind:"date_of_event", qualifying_life_event_kind_id: qle_all.id)
+  all_er_profile = FactoryGirl.create(:employer_profile)
+  all_census_ee = FactoryGirl.create(:census_employee, employer_profile: all_er_profile)
+  person_all.employee_roles.first.census_employee = all_census_ee
+  person_all.employee_roles.first.save!
+  family_all = Family.find(family_all.id)
+  Caches::PlanDetails.load_record_cache!
 end
 
 Given(/^I am already enrolled$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  empty
 end
 
 Given(/^my gender is set to male$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+  empty
 end
 
 When(/^I visit the Families Home Page$/) do
